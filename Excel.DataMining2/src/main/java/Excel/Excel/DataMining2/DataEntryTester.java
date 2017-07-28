b57326105aa7fe8ec7d4ac64dcac17f1a31d569e
L00107244 <L00107244@student.lyit.ie>
2017-07-28 09:34:58 +0100

    populate the Excel with random data

--- Excel.DataMining2/src/main/java/Excel/Excel/DataMining2/DataEntryTester.java
+++ Excel.DataMining2/src/main/java/Excel/Excel/DataMining2/DataEntryTester.java
@@ -1,68 +1,25 @@
 package Excel.Excel.DataMining2;
-
+/*
+ * Stephen Curran
+ */
 import java.io.IOException;
-import java.util.Scanner;
 
 import jxl.write.WriteException;
 
 public class DataEntryTester 
 {
-	  private static Scanner keyIn;
-	
-
 	public static void main(String [] args) throws WriteException, IOException
 	  {
-		  keyIn = new Scanner(System.in);
-         
-         
-		  System.out.println("Enter branch Number: ");
-		  int bn1 = keyIn.nextInt();
-		  System.out.println("Item Number: ");
-		  int in1 = keyIn.nextInt();
-		  keyIn.nextLine();
-		  System.out.println("item name: ");
-		  String itemname1 = keyIn.nextLine();
-		  System.out.println("Vendor: ");
-		  String ven1 = keyIn.nextLine();
-		  System.out.println("Description: ");
-		  String desc1 = keyIn.nextLine();
-		  System.out.println("Enter Location: ");
-		  String locat1 = keyIn.nextLine();
-		  System.out.println("Enter cost per item: ");
-		  double cpi1 = keyIn.nextDouble();
-		  keyIn.nextLine();
-		  System.out.println("Enter stock quanity");
-		  int sq1 = keyIn.nextInt();
-		  keyIn.nextLine();
-		  System.out.println("Total value: ");
-		  double val1 = keyIn.nextDouble();
-		  keyIn.nextLine();
-		  System.out.println("reorder level: ");
-		  int rl1 = keyIn.nextInt();
-		  System.out.println("Enter days per re order: ");
-		  int dpr1 = keyIn.nextInt();
-		  
-		  data_entries dataen = new data_entries();
-		
-		  dataen.setBranchNumber(bn1);
-		  dataen.setItemNumber(in1);
-		  dataen.setItemName(itemname1);
-		  dataen.setVendor(ven1);
-		  dataen.setDescription(desc1);
-		  dataen.setLocation(locat1);
-		  dataen.setCostPerItem(cpi1);
-		  dataen.setStockQuality(sq1);
-		  dataen.setTotalVal(val1);
-		  dataen.setReOrderLevel(rl1);
-		  dataen.setdaysPerReOrder(dpr1);
-
-	      Excel_methods test = new Excel_methods();
+		  //creates object
+		  Excel_methods test = new Excel_methods();
+		  //sets name of file
 	      test.setOutputFile("DataEntries.xls");
+	      //writes records to file
 	      test.write();
+	      //prints location
 	      System.out.println("Please check the project files for DataEntries.xls ");
 
-          }
+       }
 	  
-		  
-	   
+		     
 }
--- Excel.DataMining2/src/main/java/Excel/Excel/DataMining2/Excel_methods.java
+++ Excel.DataMining2/src/main/java/Excel/Excel/DataMining2/Excel_methods.java
@@ -1,17 +1,24 @@
 package Excel.Excel.DataMining2;
-
+/*
+ * Stephen Curran
+ */
 import jxl.write.WritableCellFormat;
 
 import java.io.File;
 
 import java.io.IOException;
 import java.util.Locale;
+import java.util.Random;
+import java.util.List;
+import jxl.CellType;
 import jxl.CellView;
 import jxl.Workbook;
 import jxl.WorkbookSettings;
 import jxl.format.UnderlineStyle;
+import jxl.read.biff.BiffException;
 import jxl.write.Label;
 import jxl.write.Number;
+import jxl.write.WritableCell;
 import jxl.write.WritableFont;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
@@ -20,7 +27,7 @@ import jxl.write.biff.RowsExceededException;
 
 
 
-public class Excel_methods extends data_entries
+public class Excel_methods
 {
 	  /*
 	   * Excel variables
@@ -28,19 +35,27 @@ public class Excel_methods extends data_entries
 	  private WritableCellFormat timesBoldUnderline;
 	  private WritableCellFormat times;
 	  private String inputFile;
+	  private static int id;
+	  private static final int max_records = 50000;
 	  //Constuctors
 	  public Excel_methods()
 	  {
-		  super();
+		 
+	  }
+	  public static int getId()
+	  {
+		  return id;
 	  }
 	  
 	  /*
 	   * Excel read and write methods
 	   */
+	  //method to set up the file
 	  public void setOutputFile(String inputFile)
 	  {
 	  	this.inputFile = inputFile;
 	  }
+	  //Creates the headings in the document and sets the font
 	  private void CreateLabel(WritableSheet sheet) throws WriteException
 	  {
 	      WritableFont times10pt = new WritableFont(WritableFont.TIMES, 10);
@@ -73,32 +88,34 @@ public class Excel_methods extends data_entries
 	      addCaption(sheet, 11, 0, "Days per re-order");
 	      
 	  }
+	  //Method to add a caption to the document
 		private void addCaption(WritableSheet sheet, int column, int row, String s)
 	            throws RowsExceededException, WriteException {
 	        Label label;
 	        label = new Label(column, row, s, timesBoldUnderline);
 	        sheet.addCell(label);
 	    }
-
+       //method to add ints to the document
 	    private void addNumber(WritableSheet sheet, int column, int row,
 	            Integer integer) throws WriteException, RowsExceededException {
 	        Number number;
 	        number = new Number(column, row, integer, times);
 	        sheet.addCell(number);
 	    }
+	    //Second int method
 	    private void addInts(WritableSheet sheet, int column, int row, int intin) throws RowsExceededException, WriteException
 	    {
 	    	Number number;
 	    	number = new Number(column, row, intin, times);
 	    	sheet.addCell(number);
 	    }
+	    //method to add decimals to the document
 	    private void addDeciamls(WritableSheet sheet, int column, int row, double d) throws WriteException, RowsExceededException {
 	        Number number;
 	        number = new Number(column, row, d, times);
 	        sheet.addCell(number);
 	    }
-
-
+       //Method to add Strings to the document
 	    private void addLabel(WritableSheet sheet, int column, int row, String s)
 	            throws WriteException, RowsExceededException 
 	    {
@@ -106,28 +123,53 @@ public class Excel_methods extends data_entries
 	        label = new Label(column, row, s, times);
 	        sheet.addCell(label);
 	    }
+	    public static double randDouble(double minimum, double maximum) {
+	      
+	        double minin = Math.min(minimum, maximum);
+	        double max = Math.max(minimum, maximum);
+	        return minin + (Math.random() * (maximum - minimum));
+	    }
+	    //Method to populate the document
 	  public void CreateContent(WritableSheet sheet) throws WriteException
 	  {
-		  
-
-		  for(int i=1; i<2000; i++)
+		  //Object and variable
+		  Random randomGenerator = new Random();
+		  int randomInt;
+		  double Min = 20.0;
+		  double Max = 900.0;
+		  Double randomDouble = Math.random();
+		  String random2;
+          //iterate to add 50,000 records to Excel document
+		  for(int i=1; i<max_records; i++)
 		  {
-		   addNumber(sheet, 0, i, i + data_entries.getId());
-		   addInts(sheet, 1, i, data_entries.getBranchNumber());
-		   addInts(sheet, 2, i,data_entries.getItemNumber());
-		   addLabel(sheet, 3, i, data_entries.getItemName() );
-		   addLabel(sheet, 4, i, data_entries.getVendor());
-		   addLabel(sheet, 5, i, data_entries.getDescription());
-		   addLabel(sheet, 6, i, data_entries.getLocation());
-		   addDeciamls(sheet, 7, i, data_entries.getCostPerItem());
-		   addInts(sheet, 8, i,data_entries.getStockQuanitiy());
-		   addDeciamls(sheet, 9, i, data_entries.getTocalValue());
-		   addInts(sheet, 10, i, data_entries.getReorderLevel());
-		   addInts(sheet, 11, i, data_entries.getDaysPerReOrder());
+		   String[] list = new String[] { "USA", "Canada",
+			            "Ireland", "England", "Austrailia", "Germany", "France" };
+		   String[] list2 = new String[] {"Sumsung S8", "IPhone7", "lenovo thinkpad",
+			    		"Coca-Cola", "Chromecast", "Micro wave", "Toilet" };
+		   String[] list3 = new String[] {"ma", "ln", "vn", "es", "ty", "sc", "cv"};
+		   String[] list4 = new String[] {"smaple descrip1", "sample descrip2", "sample descrip3"};
+		   int randomLocation = randomGenerator.nextInt(list.length);
+		   int randomItemName = randomGenerator.nextInt(list2.length);
+		   int randomVendor = randomGenerator.nextInt(list3.length);
+		   int randomDescription = randomGenerator.nextInt(list4.length);
+		   addNumber(sheet, 0, i, i + Excel_methods.getId());
+		   addInts(sheet, 1, i, randomInt = randomGenerator.nextInt(100));
+		   addInts(sheet, 2, i,randomInt = randomGenerator.nextInt(100));
+		   addLabel(sheet, 3, i, random2 = (String) (list2[randomItemName]) );
+		   addLabel(sheet, 4, i,  random2 = (String) (list3[randomVendor]));
+		   addLabel(sheet, 5, i, random2 = (String) (list4[randomDescription]));
+		   addLabel(sheet, 6, i, random2 = (String) (list[randomLocation]));
+		   addDeciamls(sheet, 7, i, Excel_methods.randDouble(Min, Max));
+		   addInts(sheet, 8, i, randomInt = randomGenerator.nextInt(100));
+		   addDeciamls(sheet, 9, i, Excel_methods.randDouble(Min, Max));
+		   addInts(sheet, 10, i, randomInt = randomGenerator.nextInt(100));
+		   addInts(sheet, 11, i, randomInt = randomGenerator.nextInt(100));
 		  }
 
 
+
 	  }
+	  //Write the records to Excel
 	  public void write() throws IOException, WriteException
 	  {
 		File file = new File(inputFile);
@@ -144,4 +186,5 @@ public class Excel_methods extends data_entries
 		workbook.close();
 		
 	  }
+
 }
